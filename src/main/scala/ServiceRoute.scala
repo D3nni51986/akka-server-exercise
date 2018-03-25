1922a6b5681f858ae117c7ae619150c08648fcd4
Dennis <dennis@m-m-g.com>
2018-03-25 18:02:06 +0300

    route commit

--- src/main/scala/ServiceRoute.scala
+++ src/main/scala/ServiceRoute.scala
@@ -1,13 +1,37 @@
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.{Directive1, Rejection}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.Materializer
 
 trait ServiceRoute {
-  lazy val route = path("hello") {
-    get {
-      complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+
+  val route = {
+    pathPrefix("service" /){
+      get{
+        extractEventType{
+          event => complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Get ${event}</h1>"))
+        }
+      }~post{
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>Post</h1>"))
+      }
     }
   }
+
+  private def extractEventType:Directive1[String] = {
+    parameter('countByEventType.?).flatMap{
+      case Some(eventT) => provide(eventT)
+      case _          => {
+        extractUri.flatMap(uri => reject(new ServiceRejection(s"${uri}")))
+      }
+    }
+  }
+
+  class ServiceRejection(val error:String) extends Throwable with Rejection {
+    override def toString = {
+      s"Error ${error}"
+    }
+  }
+
 
   protected implicit def materializer: Materializer
 
